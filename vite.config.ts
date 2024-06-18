1b1c431a3cc220bc924781f10d267148ff050ac9
Joshua Jacobson <jmjpro@gmail.com>
2024-06-18 18:15:25 +0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<REPO>/',
+  base: '/intuit-fe-dev/',
   /* server: {
     proxy: {
       '/example-turnstile': {
