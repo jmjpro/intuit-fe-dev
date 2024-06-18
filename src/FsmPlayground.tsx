0bcc1f448c19cf5a37d9761248d63f1cbf476235
Joshua Jacobson <jmjpro@gmail.com>
2024-06-18 18:50:23 +0300

    formatting

--- src/FsmPlayground.tsx
+++ src/FsmPlayground.tsx
@@ -56,16 +56,20 @@ function FsmPlayground() {
       <div>{message}</div>
 
       <main className={styles.main}>
-        <div className={styles.directions}><Info size={16}/>Add states, add transitions between the states, then click transition buttons to run transitions and notice how the state changes</div>
-        
+        <div className={styles.directions}>
+          <Info size={16} />
+          Add states, add transitions between the states, then click transition
+          buttons to run transitions, and notice how the state changes
+        </div>
+
         <FsmBuilder fsm={fsm} setFsm={setFsm} />
-        
+
         <hr />
-        
+
         <FsmViz fsm={fsm} handleRunTransition={handleRunTransition} />
-        
+
         <hr />
-        
+
         <div>
           <button onClick={handleResetState}>
             Reset state <Trash size={16} />
